329c9ddd2c2066e9a2b44173ddb345adb9da573d
Ge Will <531sunlight@gmail.com>
2023-08-21 17:47:03 +0800

    新增 启动时检查会员

--- JColors/JColorsApp.swift
+++ JColors/JColorsApp.swift
@@ -9,12 +9,27 @@ import SwiftUI
 
 @main
 struct JColorsApp: App {
+  @AppStorage(UserDefaultsKeys.isPro.rawValue) var isPro: Bool = false
+  @AppStorage(UserDefaultsKeys.lastCheckProDate.rawValue) var lastCheckProDate: TimeInterval = Date().yesterday.unixTimestamp
+
   var body: some Scene {
     WindowGroup {
       ContentView()
         .onAppear {
           IAPManager.shared.configure()
+          checkPro()
         }
+    }
+  }
+
+  // MARK: - private methods
+
+  func checkPro() {
+    if Date().yesterday.unixTimestamp >= lastCheckProDate {
+      IAPManager.shared.checkProLifetime { isPro in
+        self.isPro = isPro
+        lastCheckProDate = Date().unixTimestamp
+      }
     }
   }
 }
